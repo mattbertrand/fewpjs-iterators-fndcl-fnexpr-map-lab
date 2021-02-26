1788ce866cf0e9df3585c8c51cdeb6f3edf6c54e
github username <github email address>
2021-02-26 14:18:51 -0800

    commit tree 57eb4dcff4bbc71b7c8a2e7b068bfb33f2b3d079

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+const titleCased = (input) => {
+  return tutorials.map(sentence => {
+    const tokens = sentence.split(' ')
+    const capitalizedTokens = tokens.map( token => token.charAt(0).toUpperCase() + token.slice(1))
+    const response = capitalizedTokens.join(' ')
+    return response
+  })
 }
